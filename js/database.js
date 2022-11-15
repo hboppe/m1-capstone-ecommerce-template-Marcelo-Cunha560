b38807e51adbd9b86840e569a0021806ed017f79
Marcelo-Cunha560 <113524572+Marcelo-Cunha560@users.noreply.github.com>
2022-11-15 11:09:20 -0400

    new commit

--- js/database.js
+++ js/database.js
@@ -3,17 +3,17 @@
 const data = [
   {
     id: 1,
-    img: "../img/acessorio-1",
+    img: "../img/acessorio-1.svg.jpeg",
     nameItem: "Edifier FONE DE OUVIDO BLUETOOTH",
     description:
-      "Emparelhamento com 2 aparelhos!!! Transmissão Estável. O Bluetooth V5.1 garante uma transmissão mais rápida, em um alcance maior e uma conexão mais estável. ...",
+    "Emparelhamento com 2 aparelhos!!! Transmissão Estável. O Bluetooth V5.1 garante uma transmissão mais rápida, em um alcance maior e uma conexão mais estável. ...",
     value: 200,
     addCart: "Adicionar ao carrinho",
     tag: ["Acessorios"],
   },
   {
     id: 2,
-    img: "../img/acessorio2",
+    img: "../img/acessorio2.jpeg",
     nameItem: "Fone de Ouvido JBL C300SI",
     description:
       "O novo JBL C300SI é um fone de ouvido on-ear potente, dinâmico e ultraleve, com alto-falantes potentes...",
@@ -23,7 +23,7 @@ const data = [
   },
   {
     id: 3,
-    img: "../img/acessorio3",
+    img: "../img/acessorio3.jpeg",
     nameItem: "Caixa de Som Bluetooth JBL Charge 5 40W Preta",
     description:
       "JBL Charge 5 oferece o ousado JBL Original Pro Sound, com driver de longa excursão otimizado, tweeter separado e dois radiadores de graves JBL. ...",
@@ -33,7 +33,7 @@ const data = [
   },
   {
     id: 4,
-    img: "../img/elden-ring",
+    img: "../img/elden-ring.jpeg",
     nameItem: "Elden Ring",
     description:
       "A Ordem Áurea foi quebrada. Levante-se, Maculado, e seja guiado pela graça para empunhar o poder do Anel Prístino....",
@@ -43,7 +43,7 @@ const data = [
   },
   {
     id: 5,
-    img: "../img/god-ofwar-ragnarok",
+    img: "../img/god-ofwar-ragnarok.jpeg",
     nameItem: "God of War Ragnarök",
     description:
       "Embarque em uma jornada épica e comovente onde Kratos e Atreus lutam entre o desejo de manterem-se unidos ou separar-se...",
@@ -53,7 +53,7 @@ const data = [
   },
   {
     id: 6,
-    img: "../img/mbappe",
+    img: "../img/mbappe.jpeg",
     nameItem: "FIFA 23",
     description:
       "FIFA 23 traz ainda mais da emoção do futebol e realismo aos gramados do Maior Jogo do Mundo....",
@@ -63,7 +63,7 @@ const data = [
   },
   {
     id: 7,
-    img: "../img/nintendo",
+    img: "../img/nintendo.jpeg",
     nameItem: "Console Nintendo Switch",
     description:
       "Adquira o console de videogames que lhe possibilita jogar seus jogos favoritos, onde você quiser e da maneira que preferir...",
@@ -73,7 +73,7 @@ const data = [
   },
   {
     id: 8,
-    img: "../img/ps5",
+    img: "../img/ps5.jpeg",
     nameItem: "Console PlayStation 5",
     description:
       "Entre em sua conta para a PlayStation Network e acesse a PlayStation Store para comprar e baixar jogos...",
@@ -83,7 +83,7 @@ const data = [
   },
   {
     id: 9,
-    img: "../img/xbox",
+    img: "../img/xbox.jpeg",
     nameItem: "Console Xbox Series S",
     description:
       "Xbox series s, nosso xbox menor e mais elegante de todos os tempos, Alterne entre os jogos com facilidade...",
--- js/index.js
+++ js/index.js
@@ -14,22 +14,76 @@ function listaVitrine(data) {
         }
     }
 }
-let product1 = document.querySelector('.product-1');
-let product2 = document.querySelector('.product-2');
-let product3 = document.querySelector('.product-3');
+
+listaVitrine(data)
+console.log(arrAcessorio)
+console.log(arrJogos)
+console.log(arrConsoles)
 
 
-function adicionaCard(lista, section){
-    for (let i = 0; i < lista.length; i++){
-        
-    }
+let product1 = document.querySelector('.products-1');
+let product2 = document.querySelector('.products-2');
+let product3 = document.querySelector('.products-3');
+
+for (let i = 0; i < arrAcessorio.length; i++){
+    let liDocumento = document.createElement('li');
+    liDocumento.classList.add('card');
+
+    let imagemProduto = document.createElement('img')
+    imagemProduto.src = `${arrAcessorio[i].img}`
+
+    let pTag = document.createElement('p');
+    pTag.innerHTML = `${arrAcessorio[i].tag[0]}`
+    pTag.classList.add('tag');
+
+    let titulo3 = document.createElement('h3')
+    titulo3.innerText = `${arrAcessorio[i].nameItem}`
+    console.log(titulo3)
+
+    let descricao = document.createElement('p');
+    descricao.innerText = `${arrAcessorio[i].description}`
+    
+    let valor = document.createElement('p');
+    valor.innerText = `R$ ${arrAcessorio[i].value.toFixed(2)}`
+
+    let addcarinho = document.createElement('a');
+    addcarinho.innerText = `${arrAcessorio[i].addCart}`
+
+    liDocumento.append(imagemProduto ,pTag, titulo3, descricao, valor, addcarinho);
+
+    product1.appendChild(liDocumento)
+}
+
+for (let i = 0; i < arrJogos.length; i++){
+    let liDocumento = document.createElement('li');
+    liDocumento.classList.add('card');
+
+    let imagemProduto = document.createElement('img')
+    imagemProduto.src = `${arrJogos[i].img}`
+
+    let pTag = document.createElement('p');
+    pTag.innerHTML = `${arrJogos[i].tag[0]}`
+    pTag.classList.add('tag');
+
+    let titulo3 = document.createElement('h3')
+    titulo3.innerText = `${arrJogos[i].nameItem}`
+    console.log(titulo3)
+
+    let descricao = document.createElement('p');
+    descricao.innerText = `${arrJogos[i].description}`
+    
+    let valor = document.createElement('p');
+    valor.innerText = `R$ ${arrAcessorio[i].value.toFixed(2)}`
+
+    let addcarinho = document.createElement('a');
+    addcarinho.innerText = `${arrJogos[i].addCart}`
+
+    liDocumento.append(imagemProduto ,pTag, titulo3, descricao, valor, addcarinho);
+
+    product2.appendChild(liDocumento)
 }
 
 
 
 
 
-listaVitrine(data)
-console.log(arrAcessorio)
-console.log(arrJogos)
-console.log(arrConsoles)
